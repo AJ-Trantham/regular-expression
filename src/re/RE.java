3ab062a88ce08db702aba1ccf93aa1ad9c0d9797
AJ Trantham <ajtrantham@u.boisestate.edu>
2021-04-25 20:27:25 -0600

    basic,paren, fact and tc1 and tc3 working

--- src/re/RE.java
+++ src/re/RE.java
@@ -9,7 +9,7 @@ import java.util.Set;
 /**
  * regex: term "|" regex | term
  * term: factor
- * factor: ground | ground*factor | ground factor
+ * factor: ground | ground*factor
  * ground: base ground| base
  * base: char | char base | "(" regex ")"
  * char: a | b | e
@@ -23,6 +23,7 @@ public class RE implements REInterface {
     private String currentState;
     private String finalState; // all states which should be a final state should map to this state with an e transition
     private int stateNameCount;
+    private String startOfRegex;
 
     public RE(String regEx) {
         this.regexString = regEx;
@@ -38,6 +39,7 @@ public class RE implements REInterface {
         nfa.addStartState(nextStateName());
         currentState = nfa.getStartState().getName();
         finalState = "final";
+        startOfRegex = "";
     }
 
     @Override
@@ -45,20 +47,21 @@ public class RE implements REInterface {
 
 
         // this method will be our parser
-
+        RE regex = regex();
 
 
         // parse the regex - TODO: change to walk through entire grammar
-        while (more()) {
-            previousState = currentState;
-            RE character = character();
-            currentState = nextStateName();
-            nfa.addState(currentState);
-            nfa.addTransition(previousState, character.toString().charAt(0), currentState);
-        }
+//        while (more()) {
+//            previousState = currentState;
+//            RE regex = regex();
+//            currentState = nextStateName();
+//            nfa.addState(currentState);
+//            nfa.addTransition(previousState, regex.toString().charAt(0), currentState);
+//        }
+//        nfa.addFinalState(finalState);
+//        nfa.addTransition(currentState, 'e', finalState);
         nfa.addFinalState(finalState);
         nfa.addTransition(currentState, 'e', finalState);
-
         return nfa;
     }
 
@@ -79,23 +82,81 @@ public class RE implements REInterface {
     }
 
     private RE regex () {
-        return null;
+        //  (a | b) | b
+        String splitState = currentState;
+        RE term = term();
+        if (more() && peek() == '|') {
+            eat('|');
+            String mergeState = nextStateName();
+            nfa.addState(mergeState);
+            nfa.addTransition(currentState, 'e', mergeState);
+            currentState = splitState;
+            term = regex();
+            nfa.addTransition(currentState,'e',mergeState);
+            currentState = mergeState;
+        }
+        return term;
     }
 
     private RE term() {
-        return null;
+        return factor();
     }
 
     private RE factor() {
-        return null;
+        //a*
+        // (ab)*a*
+        //String beginingOfRepetition = currentState;
+        startOfRegex = ""; // set this to empty so ground can set it to the state where we begin processing a regex
+        RE ground = ground();
+        String endOfRepetition = currentState;
+        // problem here don't want
+        if (more() && peek() == '*') { // changed from while but we should only ever look for one star here and two stars doesn't make sense
+            eat('*');
+            String beginingOfRepitition = startOfRegex.equals("") ? previousState : startOfRegex;
+            nfa.addTransition(endOfRepetition, 'e', beginingOfRepitition); // either the previous state or a whole regex ()
+            nfa.addTransition(beginingOfRepitition, 'e', endOfRepetition);
+            if (more()) { // this should be here?
+                ground = factor();
+                //nfa.addTransition(endOfRepetition, 'e',ground.nfa.getStartState().getName());
+            }
+        }
+
+        return ground;
     }
 
     private RE ground() {
-        return null;
+
+        RE base = base();
+        while(more() && peek() != '|' && peek() != '*' && peek() != ')') {
+            base = ground();
+        }
+        return base;
     }
 
     private RE base() {
-        return null;
+        switch(peek()) {
+            case '(': {
+                eat('(');
+                startOfRegex = currentState;
+                RE regex = regex();
+                eat(')');
+                return regex;
+            }
+            // for char and car base production rules aa(ab)*  aaa
+            default:
+                // for single char
+                previousState = currentState;
+                RE character = character();
+                currentState = nextStateName();
+                nfa.addState(currentState);
+                nfa.addTransition(previousState, character.toString().charAt(0), currentState);
+                if (more() && peek() != '|' && peek() != '*' && peek() != ')') { // continue to process if string of chars
+                    character = base();
+                }
+
+                return character; // what needs to be returned here??
+        }
+
     }
 
     private RE character() {
